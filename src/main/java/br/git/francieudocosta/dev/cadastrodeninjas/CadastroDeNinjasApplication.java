cd982d2823592076a12f163639ee0457088e6551
francieudocosta <francieudocosta@gmail.com>
2025-07-25 19:05:28 -0300

    first commit

---
+++ src/main/java/br/git/francieudocosta/dev/cadastrodeninjas/CadastroDeNinjasApplication.java
@@ -0,0 +1,12 @@
+package br.git.francieudocosta.dev.cadastrodeninjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNinjasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadastroDeNinjasApplication.class, args);
+    }
+}
