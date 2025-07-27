82f086be94e934f75c25eee449aa7005b457c9d6
francieudocosta <francieudocosta@gmail.com>
2025-07-27 11:01:50 -0300

    feat: Atualiza o controller com mock de dados

--- src/main/java/br/git/francieudocosta/dev/cadastrodeninjas/ninjas/controllers/NinjaController.java
+++ src/main/java/br/git/francieudocosta/dev/cadastrodeninjas/ninjas/controllers/NinjaController.java
@@ -1,16 +1,38 @@
 package br.git.francieudocosta.dev.cadastrodeninjas.ninjas.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/ninjas")
 public class NinjaController {
 
-    @GetMapping
-    public String boasVindas(){
+    @PostMapping
+    public String criarNinja() {
 
-        return "Boas Vindas";
+        return "Ninja Criada com Sucesso!";
+    }
+
+    @GetMapping
+    public String mostrarTodosNinjas(){
+
+        return "Mostrar todos os ninjas";
+    }
+
+    @GetMapping("/{id}")
+    public String mostrarNinjaPorId(@PathVariable Long id) {
+
+        return "Ninja encontrada com sucesso!";
+    }
+
+    @PutMapping("/{id}")
+    public  String atualizarNinja(Long id) {
+
+        return "Ninja atualizada com sucesso!";
+    }
+
+    @DeleteMapping("/{id}")
+    public  String deletarNinjaId(Long id) {
+
+        return "Ninja deletado com sucesso!";
     }
 }
